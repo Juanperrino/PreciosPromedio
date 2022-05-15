3c0d02b974a1b7f5a1d3aac97d3d9482f4fde2e6
jperrino <juanperrino90@gmail.com>
2022-05-15 17:36:05 -0300

    Correcciones

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ input.addEventListener("keyup", (event) => {
     autocompletado_resultado.style.display = "block";
     let key = (event.target.value);
 
-    if (key.length > 1) {
+    if (key.length > 1){
         //buscar paises
         buscar(key);
     }
@@ -19,20 +19,20 @@ input.addEventListener("keyup", (event) => {
 
 const buscar = (key) => {
     fetch(`https://restcountries.com/v3.1/name/${key}`)
-        .then(res => res.json())
-        .then(data => {
+        .then( res => res.json() )
+        .then( data => {
             // console.log(data);
-            if (Array.isArray(data)) {
+            if(Array.isArray(data)){
 
                 construir_lista(data.map((item) => {
                     return item.name.common;
                 }));
             }
-            else {
+            else{
                 construir_lista()
             }
         })
-        .catch(err => { console.log(err) })
+        .catch(err => {console.log(err)})
 }
 
 const construir_lista = (items = []) => {
@@ -47,7 +47,7 @@ const construir_lista = (items = []) => {
 }
 
 autocompletado_resultado.addEventListener("click", (e) => {
-    if (e.target && e.target.nodeName == "li") {
+    if(e.target && e.target.nodeName == "LI"){
         input.value = e.target.innerHTML;
         construir_lista();
     }
@@ -60,14 +60,14 @@ autocompletado_resultado.addEventListener("click", (e) => {
 let productosCarniceria = ['ASADO', 'VACIO', 'PALETA', 'NALGA', 'CUADRIL']
 let productosVerduleria = ['TOMATE', 'LECHUGA', 'CEBOLLA', 'PAPA']
 
-function convertString(array) {
+function convertString(array){
     return JSON.stringify(array)
 }
 
 let productosCarniceriaJson = convertString(productosCarniceria)
 let productosVerduleriaJson = convertString(productosVerduleria)
 
-function guardarLocalStorage(key, value) {
+function guardarLocalStorage(key, value){
     return localStorage.setItem(key, value)
 }
 
@@ -77,10 +77,10 @@ guardarLocalStorage("productosVerduleriaJson", productosVerduleriaJson)
 arry = JSON.parse(localStorage.getItem("productosVerduleriaJson"))
 
 let search = document.getElementById("search")
-search.addEventListener('click', () => {
-    for (index of arry) {
-        // let produts = console.log(index)
-    }
+search.addEventListener('click', ()=>{
+    for (index of arry){
+    // let produts = console.log(index)
+}
 });
 
 
@@ -101,35 +101,35 @@ let contador = 0;
 
 
 
-function cambio() {
+function cambio(){
 
-    if (contador == 0) {
+    if (contador==0){
         btn.classList.add('azul')
         btn2.classList.remove('azul')
-        contador = 1;
+        contador=1;
     }
-    else {
+    else{
         btn.classList.remove('azul')
-        contador = 0;
+        contador=0;
     }
 }
 
-function cambio2() {
+function cambio2(){
 
-    if (contador == 0) {
+    if (contador==0){
         btn2.classList.add('azul')
         btn.classList.remove('azul')
-        contador = 1;
+        contador=1;
     }
-    else {
+    else{
         btn2.classList.remove('azul')
-        contador = 0;
+        contador=0;
     }
 }
 
 
-btn.addEventListener('click', cambio, true);
-btn2.addEventListener('click', cambio2, true);
+btn.addEventListener('click',cambio,true);
+btn2.addEventListener('click',cambio2,true);
 
 
 // fIN PASO 1 SELECCIONA EL RUBRO
@@ -146,44 +146,44 @@ let zonas = ['CABA', 'Zona Norte', 'Zona Sur', 'Zona Este', 'Zona Oeste'];
 let localidades = ['C1', 'C2', 'C3', 'C4', 'C5', 'N1', 'N2', 'N3', 'N4', 'N5', 'S1', 'S2', 'S3', 'S4', 'S5', 'E1', 'E2', 'E3', 'E4', 'E5', 'O1', 'O2', 'O3', 'O4', 'O5',];
 
 
-function verLugares(arreglo, lugar) {
+function verLugares(arreglo, lugar){
     let elementos = '<option selected disables></option>'
 
-    for (let i = 0; i < arreglo.length; i += 1) {
-        elementos += '<option value="' + arreglo[i] + '">' + arreglo[i] + '</option>'
+    for (let i=0; i < arreglo.length; i+=1){
+        elementos += '<option value="' + arreglo[i] +'">' + arreglo[i] +'</option>'
     }
 
-    lugar.innerHTML = elementos
+    lugar.innerHTML = elementos 
 }
 
 verLugares(zonas, zona);
 
 
-function filtrar(array, inicio, fin, lugar) {
+function filtrar(array, inicio, fin, lugar){
     let filtro = array.slice(inicio, fin);
     verLugares(filtro, lugar);
 }
 
 
-zona.addEventListener('change', function () {
-    let valor = zona.value
+zona.addEventListener('change', function(){
+    let valor = zona.value 
 
-    switch (valor) {
+    switch(valor){
         case 'CABA':
             filtrar(localidades, 0, 5, localidad);
-            break
+        break
         case 'Zona Norte':
             filtrar(localidades, 5, 10, localidad);
-            break
+        break
         case 'Zona Sur':
             filtrar(localidades, 10, 15, localidad);
-            break
+        break
         case 'Zona Este':
             filtrar(localidades, 15, 20, localidad);
-            break
+        break
         case 'Zona Oeste':
             filtrar(localidades, 20, 25, localidad);
-            break
+        break
     }
 
 })
